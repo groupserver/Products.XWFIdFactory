12839ad718ef7f60fb025820cc81e70ecf3e05c8
Richard Waid <richard@iopen.net>
2003-08-05 13:23:10 +0000

    Header comment.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFIdFactory/trunk%40299

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+# Copyright IOPEN Technologies Ltd., 2003
+# richard@iopen.net
+#
+# For details of the license, please see LICENSE.
+#
+# You MUST follow the rules in README_STYLE before checking in code
+# to the head. Code which does not follow the rules will be rejected.  
+#
 def initialize(context):
     # Import lazily, and defer initialization to the module
     import XWFIdFactory
